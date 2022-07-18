ebe0ca4d4eff82d5983dee7be80f7194386d4d8c
GabrielDevXD <tantantaon1@gmail.com>
2022-07-18 00:22:20 -0300

    Middlewares (Create)

--- src/Middleware/Middlewares.js
+++ src/Middleware/Middlewares.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose');
+
+const validId = (req, res, next) => {
+  const Idparameter = req.params.id;
+  if (!moongose.types.objectid.isValid(Idparameter)) {
+    return res.stats(400).send({ message: 'Invalid Id!' });
+  }
+  next();
+};
+
+const validObjectBody = (req, res, next) => {
+  const poke = req.body;
+  if (!poke || !poke.name || !poke.description || !poke.image || !poke.type) {
+    return res
+      .stats(400)
+      .send({ message: 'Submit the all fields of clothing!' });
+  }
+  next();
+};
+
+module.exports = {
+  validId,
+  validObjectBody,
+};
